da0ffef88645139fd45a598612d30b27ec0bfa99
Georgina Rojas <ginarojas29@gmail.com>
2021-04-16 22:19:58 -0300

    Were created the divider and text blocks.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { Text, Box, Divider, Flex } from "@chakra-ui/react";
+
 import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 import { NavBar } from "../components/NavBar";
@@ -9,6 +11,31 @@ export default function Home() {
       <Header href="/" />
       <Banner />
       <NavBar />
+      <Box
+        width={["60px", "90px"]}
+        align="center"
+        justify="center"
+        margin="0 auto"
+        
+      >
+        <Divider borderColor="dark.text" borderWidth="2px" />
+      </Box>
+
+      <Text
+        width={["297px", "839px"]}
+        height={["54px", "101px"]}
+        fontSize={["20px", "36px"]}
+        fontWeight="500"
+        lineHeight={["30px", "54px"]}
+        color="dark.text"
+        align="center"
+        margin="0 auto"
+        marginTop={["24px", "48px"]}
+        
+      >
+        Vamos nessa? <br />
+        Então escolha seu continente
+      </Text>
     </Flex>
   );
 }
